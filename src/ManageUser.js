03e2104c7c19aef8dbab42861a654f8e99accf7a
varsha bhaskar <vbhaskar@paycor.com>
2019-09-12 12:03:27 -0400

    handling loading states

--- src/ManageUser.js
+++ src/ManageUser.js
@@ -16,27 +16,46 @@ function ManageUser(props) {
   const setUser = userState[1]; */
 
   //can be simplified as
-  const [user, setUser] = useState(newUser);
+  const [user, setUser] = useState(newUser); //newUSer is the default object
+  const [errors, setErrors] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+  const [isFormSubmitted, setIsFormSubmitted] = useState(false);
   //user is same as a state prop in class like this.state.user
   //setUser is same as setSate in class like this.setState
 
   useEffect(() => {
+    let mounted = true;
     //If it is edit, then we do this
     if (props.match.params.userId) {
       userApi.getUserById(props.match.params.userId).then(user => {
-        setUser(user);
+        //this is to prevent error which occurs when u navigate to another page when api called is in progress
+        if (mounted) {
+          setUser(user);
+          setIsLoading(false);
+        }
       });
-    }
+    } else setIsLoading(false); //If adding, nothing to load
+
+    //called when componenet is unmounting.
+    return () => (mounted = false);
   }, [props.match.params.userId]); //this will get called every time it renders and only when the second param value changes
 
   function handleSave(savedUser) {
     props.history.push("/users"); // will redirect to a relative url based on host name. Another option to Redirect
-    debugger;
     toast.success(savedUser.name + " saved! 👍");
   }
-
+  function isValid() {
+    const _errors = {};
+    if (!user.name) _errors.name = "Name is required";
+    if (!user.hairColor) _errors.hairColor = "Hair Color is required";
+    setErrors(_errors);
+    // if errors object still doesn't have any properties, then there are no errors
+    return Object.keys(_errors).length === 0;
+  }
   function saveUser(event) {
     event.preventDefault(); //prevents posting back to server
+    if (!isValid()) return;
+    setIsFormSubmitted(true);
     user.id
       ? userApi.editUser(user).then(handleSave)
       : userApi.addUser(user).then(handleSave);
@@ -50,6 +69,7 @@ function ManageUser(props) {
     setUser(userCopy);
   }
 
+  if (isLoading) return "...Loading 🔃";
   return (
     <>
       <h1>Manage User</h1>
@@ -61,6 +81,7 @@ function ManageUser(props) {
           value={user.name}
           onChange={handleChange}
           label="Name"
+          error={errors.name}
         ></Input>
 
         <Input
@@ -70,9 +91,14 @@ function ManageUser(props) {
           value={user.hairColor}
           onChange={handleChange}
           label="Hair color"
+          error={errors.hairColor}
         ></Input>
 
-        <input type="submit" value="Save User"></input>
+        <input
+          type="submit"
+          disabled={isFormSubmitted ? "disabled" : null}
+          value={isFormSubmitted ? "Saving..." : "Save User"}
+        ></input>
       </form>
     </>
   );
--- src/reusable/Input.js
+++ src/reusable/Input.js
@@ -1,6 +1,14 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { errorColor } from "../styles";
 
+const errorStyle = {
+  color: errorColor,
+  fontWeight: "bold"
+};
+const inputErrorStyle = {
+  border: "solid 1px " + errorColor
+};
 function Input(props) {
   return (
     <div>
@@ -12,7 +20,9 @@ function Input(props) {
         name={props.name} // this is the property we wanna set onChange. can name it anything
         value={props.value}
         onChange={props.onChange}
+        style={props.error && inputErrorStyle}
       ></input>
+      {props.error && <p style={errorStyle}>{props.error}</p>}
     </div>
   );
 }
